42940e1f6671464ff05076640a69fd001eb3db2a
unknown <ompreetam317@gmail.com>
2020-10-26 18:22:21 +0530

    [Preetam]Added: UC1 Calculate Fare

--- src/test/java/cabinvoicegenerator/CabInvoiceTest.java
+++ src/test/java/cabinvoicegenerator/CabInvoiceTest.java
@@ -6,13 +6,18 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CabInvoiceTest {
-	
+
 	@Before
-	public void initialize(){
+	public void initialize() {
 		System.out.println("Welcome to cab invoice generator test");
 	}
-	 @Test
-	public void test() {
-		
+
+	@Test
+	public void givenDistanceandTnme_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(25, fare, 0.0);
 	}
 }
