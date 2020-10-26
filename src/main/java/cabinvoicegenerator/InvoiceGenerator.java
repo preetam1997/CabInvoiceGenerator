acb7a457200355fa667254001310f188293c990b
unknown <ompreetam317@gmail.com>
2020-10-26 18:43:47 +0530

    [Preetam]Refactored:UC1 Added feature for getting minimum fare

--- src/main/java/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/cabinvoicegenerator/InvoiceGenerator.java
@@ -4,9 +4,14 @@ public class InvoiceGenerator {
 
 	private static final double MINIMUN_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
 
 	public double calculateFare(double distance, int time) {
-		return distance * MINIMUN_COST_PER_KILOMETER + time * COST_PER_TIME;
+		double totalFare =  distance * MINIMUN_COST_PER_KILOMETER + time * COST_PER_TIME;
+		if(totalFare<MINIMUM_FARE)
+			return MINIMUM_FARE;
+		else
+			return totalFare;
 	}
 
 }
--- src/test/java/cabinvoicegenerator/CabInvoiceTest.java
+++ src/test/java/cabinvoicegenerator/CabInvoiceTest.java
@@ -20,4 +20,13 @@ public class CabInvoiceTest {
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		assertEquals(25, fare, 0.0);
 	}
+	
+	@Test
+	public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(5, fare, 0.0);
+	}
 }
