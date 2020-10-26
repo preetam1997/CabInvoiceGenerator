41d6f265d248e845bdf997d98b56298dc1ddcaee
unknown <ompreetam317@gmail.com>
2020-10-26 17:49:50 +0530

    [Preetam]Added:Welcome Message

---
+++ src/test/java/cabinvoicegenerator/CabInvoiceTest.java
@@ -0,0 +1,18 @@
+package cabinvoicegenerator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CabInvoiceTest {
+	
+	@Before
+	public void initialize(){
+		System.out.println("Welcome to cab invoice generator test");
+	}
+	 @Test
+	public void test() {
+		
+	}
+}
